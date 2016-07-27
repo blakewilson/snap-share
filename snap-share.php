99e56be758d8361ade18cc87560e22dde609bcc3
Blake Wilson <me@blake.vegas>
2016-07-27 05:18:03 -0700

    Create Plugin Header Information

---
+++ snap-share.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Snap Share
+Plugin URI: http://blakewilson.me/projects/snap-share/
+Description: A simple sharing plugin for WordPress.
+Author: Blake Wilson
+Version: 1.0.0
+Author URI: http://blakewilson.me
+Donate Link: http://paypal.me/blakewilsonme
+Text Domain: snap-share
+Domain Path: /languages
+*/
